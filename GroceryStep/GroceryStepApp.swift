e4c57d106f245c3488f2194afb158dba6e4ccbd6
PhoonThetPine <thetpine254@gmail.com>
2024-07-28 11:47:53 +0800

    Initial Commit

---
+++ GroceryStep/GroceryStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStepApp.swift
+//  GroceryStep
+//
+//  Created by Phoon Thet Pine on 28/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
