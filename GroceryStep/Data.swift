d6548b046ab09e07fd35e55e399a150d2d008c37
PhoonThetPine <thetpine254@gmail.com>
2024-07-28 19:23:16 +0800

    GroceryStep - V1

--- GroceryStep/Data.swift
+++ GroceryStep/Data.swift
@@ -5,4 +5,15 @@
 //  Created by Phoon Thet Pine on 28/7/24.
 //
 
-import Foundation
+import SwiftUI
+
+var shopItems: [[Any]] = [
+    ["avocado", "Avocado", 4.00, Color.green],
+    ["banana", "Banana", 2.50, Color.yellow],
+    ["chicken", "Chicken", 12.80, Color.red],
+    ["water", "Water", 1.00, Color.blue],
+]
+
+var cartItems: [[Any]] = [
+    
+]
